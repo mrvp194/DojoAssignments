2899b7ed6287353f156936cd46c726e83f2dab25
Michael Van Pelt <mrvp194@gmail.com>
2018-03-26 15:02:58 -0500

    fixed birthday challenge

--- WebFundamentals/Javascript/birthday.js
+++ WebFundamentals/Javascript/birthday.js
@@ -1,15 +1,22 @@
-function birthday(num){
-    var sad = num +" days until my birthday.  Such a long time... :("
-    var happy = num + " DAYS"
-    if(num >= 30){
+function countdown(num){
+    while (num >= 30){
         console.log(num +" days until my birthday.  Such a long time... :(")
-    }else if(num < 30 && num > 5){
+        num--
+    }
+    while (num < 30 && num > 5){
         console.log(num + " days until my birthday.  Getting close!")
-    }else if(num <= 5 && num > 1){
+        num--
+    }
+    while (num <=5 && num > 1){
         console.log(num + " DAYS UNTIL MY BIRTHDAY!!!")
-    }else if(num === 1){
+        num--
+    }
+    while (num === 1){
         console.log("1 DAY UNTIL MY BIRTHDAY!!!")
-    }else{
+        num--
+    }
+    while (num === 0){
         console.log("HAPPY BIRTHDAY!!!!!!!")
+        break
     }
 }
