6f60293d54f9640748cee9cdf31830db16c8eb83
Michael Van Pelt <mrvp194@gmail.com>
2018-03-27 15:07:42 -0500

    fixed debugging challenge

---
+++ WebFundamentals/Javascript/debugging_jquery.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $('p, h3, li').hover(function(){
+        $(this).css('color', 'white');
+    }, function(){
+        $(this).css('color', 'black');
+    });
+
+    $('a').click(function(){
+        alert("we don't actually serve pizza...");
+    });
+
+    $('img').click(function(){
+        $(this).slideUp();
+    });
+
+    $('h5').click(function(){
+        $('img').slideDown();
+    });
+
+    $('button').click(function(){
+        var result = $('input:checked')[0].value
+        alert("Thank you for your input, we like " + result + " too!");
+    });
+})
