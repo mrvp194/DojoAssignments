cdd8c3b8be05ba1e4fa19c40a18f1dd9ce96dace
Michael Van Pelt <mrvp194@gmail.com>
2018-04-02 15:29:47 -0500

    finished compare challenge

---
+++ Python/compare.py
@@ -0,0 +1,18 @@
+def compare_lists(first, second) :
+    if first == second :
+        print "The lists are the same"
+    else :
+        print "The lists are not the same"
+
+list_one = [1,2,5,6,2]
+list_two = [1,2,5,6,2]
+compare_lists(list_one, list_two)
+list_one = [1,2,5,6,5]
+list_two = [1,2,5,6,5,3]
+compare_lists(list_one, list_two)
+list_one = [1,2,5,6,5,16]
+list_two = [1,2,5,6,5]
+compare_lists(list_one, list_two)
+list_one = ['celery','carrots','bread','milk']
+list_two = ['celery','carrots','bread','cream']
+compare_lists(list_one, list_two)
